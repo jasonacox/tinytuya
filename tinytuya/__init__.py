7c5244fe7225d70f946a7c341e770d15b47ae381
uzlonewolf <github_com@hacker-nin.com>
2022-06-14 23:56:08 -0700

    Fix importing author and version

--- tinytuya/__init__.py
+++ tinytuya/__init__.py
@@ -85,6 +85,9 @@
 """
 
 from .core import *
+from .core import __version__
+from .core import __author__
+
 from .OutletDevice import OutletDevice
 from .CoverDevice import CoverDevice
 from .BulbDevice import BulbDevice
