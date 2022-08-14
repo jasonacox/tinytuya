5601eab234a719b51a856a1c1a048c01845e7732
Jason A. Cox <jason@jasonacox.com>
2022-08-14 08:22:13 -0700

    Fix dps_to_request logic for 3.2 protocol #168

--- tinytuya/core.py
+++ tinytuya/core.py
@@ -457,7 +457,7 @@ class XenonDevice(object):
             if ver == "3.2": # 3.2 behaves like 3.3 with device22
                 self.version = 3.3  
                 self.dev_type="device22"
-                self.dps_to_request = {"1": None}
+                self.dps_to_request = self.detect_available_dps()
             time.sleep(0.5)
 
     def __del__(self):
@@ -819,7 +819,7 @@ class XenonDevice(object):
         if version == 3.2: # 3.2 behaves like 3.3 with device22
                 self.version = 3.3  
                 self.dev_type="device22"  
-                self.dps_to_request = {"1": None}
+                self.dps_to_request = self.detect_available_dps()
 
     def set_socketPersistent(self, persist):
         self.socketPersistent = persist
