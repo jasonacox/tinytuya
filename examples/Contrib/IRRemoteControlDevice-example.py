1dccc1a88141efb75dd6df8d30dc607b494b43e9
uzlonewolf <github_com@hacker-nin.com>
2022-08-31 12:53:15 -0700

    Fix the example comments to match the mirrored bits

--- examples/Contrib/IRRemoteControlDevice-example.py
+++ examples/Contrib/IRRemoteControlDevice-example.py
@@ -21,9 +21,9 @@ pronto_samsung_off = '0000 006D 0000 0022 00AC 00AC 0015 0040 0015 0040 0015 004
 pulses_samsung_on = Contrib.IRRemoteControlDevice.pronto_to_pulses( pronto_samsung_on )
 pulses_samsung_off = Contrib.IRRemoteControlDevice.pronto_to_pulses( pronto_samsung_off )
 print( 'Samsung on code:', Contrib.IRRemoteControlDevice.pulses_to_samsung( pulses_samsung_on )[0] )
-# Samsung on code: {'type': 'samsung', 'uint32': 3772815718, 'address': 224, 'data': 153, 'hex': 'E0E09966'}
+# Samsung on code: {'type': 'samsung', 'uint32': 3772815718, 'address': 7, 'data': 153, 'hex': 'E0E09966'}
 print( 'Samsung off code:', Contrib.IRRemoteControlDevice.pulses_to_samsung( pulses_samsung_off )[0] )
-# Samsung off code: {'type': 'samsung', 'uint32': 3772783078, 'address': 224, 'data': 25, 'hex': 'E0E019E6'}
+# Samsung off code: {'type': 'samsung', 'uint32': 3772783078, 'address': 7, 'data': 152, 'hex': 'E0E019E6'}
 
 # discrete on/off codes for LG
 hex_lg_on = 0x20DF23DC
@@ -31,9 +31,9 @@ hex_lg_off = 0x20DFA35C
 pulses_lg_on = Contrib.IRRemoteControlDevice.nec_to_pulses( hex_lg_on )
 pulses_lg_off = Contrib.IRRemoteControlDevice.nec_to_pulses( hex_lg_off )
 print( 'LG on code:', Contrib.IRRemoteControlDevice.pulses_to_nec( pulses_lg_on )[0] )
-# LG on code: {'type': 'nec', 'uint32': 551494620, 'address': 32, 'data': 35, 'hex': '20DF23DC'}
+# LG on code: {'type': 'nec', 'uint32': 551494620, 'address': 4, 'data': 196, 'hex': '20DF23DC'}
 print( 'LG off code:', Contrib.IRRemoteControlDevice.pulses_to_nec( pulses_lg_off )[0] )
-# LG off code: {'type': 'nec', 'uint32': 551527260, 'address': 32, 'data': 163, 'hex': '20DFA35C'}
+# LG off code: {'type': 'nec', 'uint32': 551527260, 'address': 4, 'data': 197, 'hex': '20DFA35C'}
 
 ir = Contrib.IRRemoteControlDevice( 'abcdefghijklmnop123456', '172.28.321.475', '1234567890123abc' )
 
