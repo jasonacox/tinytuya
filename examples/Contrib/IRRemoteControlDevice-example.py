87b8078a18d7d3e2ee6d12ad8334ffba0de83a69
uzlonewolf <github_com@hacker-nin.com>
2022-08-31 12:05:16 -0700

    Update example to include functions to convert to/from NEC, Samsung, and Pronto IR codes

--- examples/Contrib/IRRemoteControlDevice-example.py
+++ examples/Contrib/IRRemoteControlDevice-example.py
@@ -18,27 +18,25 @@ from time import sleep
 # discrete on/off codes for Samsung
 pronto_samsung_on = '0000 006D 0000 0022 00AC 00AC 0015 0040 0015 0040 0015 0040 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0040 0015 0040 0015 0040 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0040 0015 0015 0015 0015 0015 0040 0015 0040 0015 0015 0015 0015 0015 0040 0015 0015 0015 0040 0015 0040 0015 0015 0015 0015 0015 0040 0015 0040 0015 0015 0015 0689'
 pronto_samsung_off = '0000 006D 0000 0022 00AC 00AC 0015 0040 0015 0040 0015 0040 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0040 0015 0040 0015 0040 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0040 0015 0040 0015 0015 0015 0015 0015 0040 0015 0040 0015 0040 0015 0040 0015 0015 0015 0015 0015 0040 0015 0040 0015 0015 0015 0689'
-
 pulses_samsung_on = Contrib.IRRemoteControlDevice.pronto_to_pulses( pronto_samsung_on )
 pulses_samsung_off = Contrib.IRRemoteControlDevice.pronto_to_pulses( pronto_samsung_off )
-
 print( 'Samsung on code:', Contrib.IRRemoteControlDevice.pulses_to_samsung( pulses_samsung_on )[0] )
 print( 'Samsung off code:', Contrib.IRRemoteControlDevice.pulses_to_samsung( pulses_samsung_off )[0] )
 
 # discrete on/off codes for LG
 hex_lg_on = 0x20DF23DC
 hex_lg_off = 0x20DFA35C
-
 pulses_lg_on = Contrib.IRRemoteControlDevice.nec_to_pulses( hex_lg_on )
 pulses_lg_off = Contrib.IRRemoteControlDevice.nec_to_pulses( hex_lg_off )
-
 print( 'LG on code:', Contrib.IRRemoteControlDevice.pulses_to_nec( pulses_lg_on )[0] )
 print( 'LG off code:', Contrib.IRRemoteControlDevice.pulses_to_nec( pulses_lg_off )[0] )
 
 ir = Contrib.IRRemoteControlDevice( 'abcdefghijklmnop123456', '172.28.321.475', '1234567890123abc' )
 
-# turn the tv on
+# turn the Samsung tv on
 ir.send_button( ir.pulses_to_base64( pulses_samsung_on ) )
+# turn the LG tv on
+ir.send_button( ir.pulses_to_base64( pulses_lg_on ) )
 
 print("Press button on your remote control")
 button = ir.receive_button(timeout=15)
