f970a05eb9cdeb2342ab585c31f526cf924257b1
Jason A. Cox <jason@jasonacox.com>
2023-01-21 17:56:24 -0800

    Add Tuya doc ref link

--- tinytuya/Cloud.py
+++ tinytuya/Cloud.py
@@ -23,6 +23,10 @@
         getdevicelog(deviceid, start=[now - 1 day], end=[now], evtype="1,2,3,4,5,6,7,8,9,10", size=100, params={})
           -> when start or end are negative, they are the number of days before "right now"
              i.e. "start=-1" is 1 day ago, "start=-7" is 7 days ago
+
+ Reference
+    * https://developer.tuya.com/en/docs/cloud/device-connection-service?id=Kb0b8geg6o761 
+    
 """
 
 from .core import *
