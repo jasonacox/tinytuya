09d8698c6e89c3073116ec05bd1edb57b7943077
Jason Cox <jason@jasonacox.com>
2023-05-31 20:32:32 -0700

    Update version v1.12.8

--- tinytuya/core.py
+++ tinytuya/core.py
@@ -97,7 +97,7 @@ except ImportError:
 # Colorama terminal color capability for all platforms
 init()
 
-version_tuple = (1, 12, 7)
+version_tuple = (1, 12, 8)
 version = __version__ = "%d.%d.%d" % version_tuple
 __author__ = "jasonacox"
 
