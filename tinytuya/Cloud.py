7bba52e899cbb7278aa598641182ef6683363ad1
Jason A. Cox <jason@jasonacox.com>
2023-07-04 15:34:04 -0700

    Fix logic to ignore icon and last_ip

--- tinytuya/Cloud.py
+++ tinytuya/Cloud.py
@@ -487,7 +487,7 @@ class Cloud(object):
                 continue
             is_same = True
             for k in DEVICEFILE_SAVE_VALUES:
-                if (k not in old) or (k in dev and k != 'icon' and k != 'last_ip' and old[k] != dev[k]):
+                if k in dev and k != 'icon' and k != 'last_ip' and (k not in old or old[k] != dev[k]):
                     is_same = False
                     break
             if not is_same:
