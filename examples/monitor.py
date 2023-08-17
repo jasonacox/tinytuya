1c26c8333ca31026c71f1159303d04be402fee58
uzlonewolf <github_com@hacker-nin.com>
2023-08-17 15:58:52 -0700

    Give examples/monitor.py some much-needed love

--- examples/monitor.py
+++ examples/monitor.py
@@ -8,41 +8,49 @@
 
 """
 import tinytuya
+import time
 
 # tinytuya.set_debug(True)
 
-d = tinytuya.OutletDevice('DEVICEID', 'DEVICEIP', 'DEVICEKEY')
-d.set_version(3.3)
-d.set_socketPersistent(True)
+d = tinytuya.OutletDevice('DEVICEID', 'DEVICEIP', 'DEVICEKEY', version=3.3, persist=True)
+
+STATUS_TIMER = 30
+KEEPALIVE_TIMER = 12
 
 print(" > Send Request for Status < ")
-payload = d.generate_payload(tinytuya.DP_QUERY)
-d.send(payload)
+data = d.status()
+print('Initial Status: %r' % data)
 
 print(" > Begin Monitor Loop <")
+heartbeat_time = time.time() + KEEPALIVE_TIMER
+status_time =  None
+
+# Uncomment if you want the monitor to constantly request status - otherwise you
+# will only get updates when state changes
+#status_time = time.time() + STATUS_TIMER
+
 while(True):
-    # See if any data is available
-    data = d.receive()
+    if status_time and time.time() >= status_time:
+        # Uncomment if your device provides power monitoring data but it is not updating
+        # Some devices require a UPDATEDPS command to force measurements of power.
+        # print(" > Send DPS Update Request < ")
+        # Most devices send power data on DPS indexes 18, 19 and 20
+        # d.updatedps(['18','19','20'], nowait=True)
+        # Some Tuya devices will not accept the DPS index values for UPDATEDPS - try:
+        # payload = d.generate_payload(tinytuya.UPDATEDPS)
+        # d.send(payload)
+
+        # poll for status
+        print(" > Send Request for Status < ")
+        data = d.status()
+        status_time = time.time() + STATUS_TIMER
+        heartbeat_time = time.time() + KEEPALIVE_TIMER
+    elif time.time() >= heartbeat_time:
+        # send a keep-alive
+        data = d.heartbeat(nowait=False)
+        heartbeat_time = time.time() + KEEPALIVE_TIMER
+    else:
+        # no need to send anything, just listen for an asynchronous update
+        data = d.receive()
+
     print('Received Payload: %r' % data)
-
-    # Send keyalive heartbeat
-    print(" > Send Heartbeat Ping < ")
-    payload = d.generate_payload(tinytuya.HEART_BEAT)
-    d.send(payload)
-
-    # Uncomment if you want the monitor to constantly request status - otherwise you
-    # will only get updates when state changes
-    # print(" > Send Request for Status < ")
-    # payload = d.generate_payload(tinytuya.DP_QUERY)
-    # d.send(payload)
-
-    # Uncomment if your device provides power monitoring data but it is not updating
-    # Some devices require a UPDATEDPS command to force measurements of power.
-    # print(" > Send DPS Update Request < ")
-    # Most devices send power data on DPS indexes 18, 19 and 20
-    # payload = d.generate_payload(tinytuya.UPDATEDPS,['18','19','20'])
-    # Some Tuya devices will not accept the DPS index values for UPDATEDPS - try:
-    # payload = d.generate_payload(tinytuya.UPDATEDPS)
-    # d.send(payload)
-
-    
