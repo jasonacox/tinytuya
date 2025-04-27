bd8408bd903968802d14b7980285a22a57505ec8
Jason Cox <jason@jasonacox.com>
2025-04-26 20:43:05 -0700

    Update logging for malformed device entries in Cloud module

--- tinytuya/Cloud.py
+++ tinytuya/Cloud.py
@@ -407,7 +407,7 @@ class Cloud(object):
             for dev in oldlist:
                 # make sure dev is a dict and has an id
                 if not isinstance( dev, dict ) or 'id' not in dev:
-                    logging.warning("Skipping malformed device entry: %s", dev)
+                    log.warning("Skipping malformed device entry: %s", dev)
                     continue
                 dev_id = dev['id']
                 old_devices[dev_id] = dev
