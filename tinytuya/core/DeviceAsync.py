aae3e57e928b1cad19444df85038c62f5f736f2e
3735943886 <9522390+3735943886@users.noreply.github.com>
2025-09-14 17:39:39 +0900

    DeviceAsync class as base for v2 async support
    DeviceAsync class, adapted from the v1 XenonDevice and Device class with async-compatible methods. Serves as a foundational base for v2 async features.

--- tinytuya/core/DeviceAsync.py
+++ tinytuya/core/DeviceAsync.py
@@ -1,3 +1,6 @@
+# TinyTuya Module
+# -*- coding: utf-8 -*-
+
 import asyncio
 import binascii
 import hmac
@@ -16,7 +19,6 @@ from .message_helper import MessagePayload, TuyaMessage, pack_message, unpack_me
 from . import command_types as CT, header as H
 
 log = logging.getLogger(__name__)
-
 
 def find_device(dev_id=None, address=None):
     """Scans network for Tuya devices with either ID = dev_id or IP = address
@@ -226,7 +228,7 @@ async def device_info_async(dev_id):
         loop = asyncio.get_event_loop()
         return await loop.run_in_executor(None, device_info, dev_id)
 
-class XenonDeviceAsync(object):
+class DeviceAsync(object):
     def __init__(
             self, dev_id, address=None, local_key="", dev_type="default", connection_timeout=5,
             version=3.1, persist=False, cid=None, node_id=None, parent=None,
@@ -1281,3 +1283,181 @@ class XenonDeviceAsync(object):
 
         # create Tuya message packet
         return MessagePayload(command_override, payload)
+
+
+    #
+    # The following methods are taken from the v1 Device class and modified to be async-compatible.
+    #
+    async def set_status(self, on, switch=1, nowait=False):
+        """
+        Set status of the device to 'on' or 'off'.
+
+        Args:
+            on(bool):  True for 'on', False for 'off'.
+            switch(int): The switch to set
+            nowait(bool): True to send without waiting for response.
+        """
+        # open device, send request, then close connection
+        if isinstance(switch, int):
+            switch = str(switch)  # index and payload is a string
+        payload = self.generate_payload(CT.CONTROL, {switch: on})
+
+        data = await self._send_receive(payload, getresponse=(not nowait))
+        log.debug("set_status received data=%r", data)
+
+        return data
+
+    async def product(self):
+        """
+        Request AP_CONFIG Product Info from device. [BETA]
+
+        """
+        # open device, send request, then close connection
+        payload = self.generate_payload(CT.AP_CONFIG)
+        data = await self._send_receive(payload, 0)
+        log.debug("product received data=%r", data)
+        return data
+
+    async def heartbeat(self, nowait=True):
+        """
+        Send a keep-alive HEART_BEAT command to keep the TCP connection open.
+
+        Devices only send an empty-payload response, so no need to wait for it.
+
+        Args:
+            nowait(bool): True to send without waiting for response.
+        """
+        # open device, send request, then close connection
+        payload = self.generate_payload(CT.HEART_BEAT)
+        data = await self._send_receive(payload, 0, getresponse=(not nowait))
+        log.debug("heartbeat received data=%r", data)
+        return data
+
+    async def updatedps(self, index=None, nowait=False):
+        """
+        Request device to update index.
+
+        Args:
+            index(array): list of dps to update (ex. [4, 5, 6, 18, 19, 20])
+            nowait(bool): True to send without waiting for response.
+        """
+        if index is None:
+            index = [1]
+
+        log.debug("updatedps() entry (dev_type is %s)", self.dev_type)
+        # open device, send request, then close connection
+        payload = self.generate_payload(CT.UPDATEDPS, index)
+        data = await self._send_receive(payload, 0, getresponse=(not nowait))
+        log.debug("updatedps received data=%r", data)
+        return data
+
+    async def set_value(self, index, value, nowait=False):
+        """
+        Set int value of any index.
+
+        Args:
+            index(int): index to set
+            value(int): new value for the index
+            nowait(bool): True to send without waiting for response.
+        """
+        # open device, send request, then close connection
+        if isinstance(index, int):
+            index = str(index)  # index and payload is a string
+
+        payload = self.generate_payload(CT.CONTROL, {index: value})
+
+        data = await self._send_receive(payload, getresponse=(not nowait))
+
+        return data
+
+    async def set_multiple_values(self, data, nowait=False):
+        """
+        Set multiple indexes at the same time
+
+        Args:
+            data(dict): array of index/value pairs to set
+            nowait(bool): True to send without waiting for response.
+        """
+        # if nowait is set we can't detect failure
+        if nowait:
+            if self.max_simultaneous_dps > 0 and len(data) > self.max_simultaneous_dps:
+                # too many DPs, break it up into smaller chunks
+                ret = None
+                for k in data:
+                    ret = await self.set_value(k, data[k], nowait=nowait)
+                return ret
+            else:
+                # send them all. since nowait is set we can't detect failure
+                out = {}
+                for k in data:
+                    out[str(k)] = data[k]
+                payload = self.generate_payload(CT.CONTROL, out)
+                return await self._send_receive(payload, getresponse=(not nowait))
+
+        if self.max_simultaneous_dps > 0 and len(data) > self.max_simultaneous_dps:
+            # too many DPs, break it up into smaller chunks
+            ret = {}
+            for k in data:
+                if (not nowait) and bool(ret):
+                    await asyncio.sleep(1)
+                result = await self.set_value(k, data[k], nowait=nowait)
+                merge_dps_results(ret, result)
+            return ret
+
+        # send them all, but try to detect devices which cannot handle multiple
+        out = {}
+        for k in data:
+            out[str(k)] = data[k]
+
+        payload = self.generate_payload(CT.CONTROL, out)
+        result = await self._send_receive(payload, getresponse=(not nowait))
+
+        if result and 'Err' in result and len(out) > 1:
+            # sending failed! device might only be able to handle 1 DP at a time
+            first_dp = next(iter( out ))
+            res = await self.set_value(first_dp, out[first_dp], nowait=nowait)
+            del out[first_dp]
+            if res and 'Err' not in res:
+                # single DP succeeded! set limit to 1
+                self.max_simultaneous_dps = 1
+                result = res
+                for k in out:
+                    res = await self.set_value(k, out[k], nowait=nowait)
+                    merge_dps_results(result, res)
+        return result
+
+    async def turn_on(self, switch=1, nowait=False):
+        """Turn the device on"""
+        return await self.set_status(True, switch, nowait)
+
+    async def turn_off(self, switch=1, nowait=False):
+        """Turn the device off"""
+        return await self.set_status(False, switch, nowait)
+
+    async def set_timer(self, num_secs, dps_id=0, nowait=False):
+        """
+        Set a timer.
+
+        Args:
+            num_secs(int): Number of seconds
+            dps_id(int): DPS Index for Timer
+            nowait(bool): True to send without waiting for response.
+        """
+
+        # Query status, pick last device id as that is probably the timer
+        if dps_id == 0:
+            status = await self.status()
+            if "dps" in status:
+                devices = status["dps"]
+                devices_numbers = list(devices.keys())
+                devices_numbers.sort()
+                dps_id = devices_numbers[-1]
+            else:
+                log.debug("set_timer received error=%r", status)
+                return status
+
+        payload = self.generate_payload(CT.CONTROL, {dps_id: num_secs})
+
+        data = await self._send_receive(payload, getresponse=(not nowait))
+        log.debug("set_timer received data=%r", data)
+        return data
