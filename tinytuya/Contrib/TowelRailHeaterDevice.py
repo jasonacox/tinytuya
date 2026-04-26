100c16ed59ac593a4d0174f6ca16f159709f2284
Rob Emery <git@mintsoft.net>
2026-04-26 09:24:25 +0100

    Clarifying comments and tidying up nits

--- tinytuya/Contrib/TowelRailHeaterDevice.py
+++ tinytuya/Contrib/TowelRailHeaterDevice.py
@@ -106,7 +106,7 @@ class TowelRailHeaterDevice(Device):
         return status[self.DPS_MODE]
 
     def set_operating_mode(self, mode):
-        if mode not in ("cold", "hot", "eco","auto"):
+        if mode not in ("cold", "hot", "eco", "auto"):
             return
         self.set_value(self.DPS_MODE, mode)
 
@@ -115,18 +115,22 @@ class TowelRailHeaterDevice(Device):
         return "On" if status[self.DPS_POWER] else "Off"
 
     def tuya_duration_to_minutes(self, duration):
-        return duration * 6 # Returns 10 for 1 hour, 15 for 1 hour 30, 20 for 2 hours and so on
+        # The devices returns 10 for 1 hour, 15 for 1 hour 30, 20 for 2 hours
+        # but it seems more intuitive to use minutes as our interface
+        return duration * 6
 
     def minutes_to_tuya_duration(self, duration):
-        return int(duration / 6) # Returns 10 for 1 hour, 15 for 1 hour 30, 20 for 2 hours and so on
+        # The device expects 10 for 1 hour, 15 for 1 hour 30, 20 for 2 hours
+        # so we need to convert from minutes into this format
+        return int(duration / 6)
 
     def get_timer(self):
         status = self.status()["dps"]
         return self.tuya_duration_to_minutes(status[self.DPS_TIMER])
 
     def set_timer(self, delay):
-        if delay < 30 or delay > 8 * 60: #8 hours maximum
+        if delay < 30 or delay > 8 * 60: # 8 hours maximum
             return
-        if delay % 30 != 0
+        if delay % 30 != 0:
             return
         self.set_value(self.DPS_TIMER, self.minutes_to_tuya_duration(delay))
